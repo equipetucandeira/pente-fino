4c581d746616d688e8d5231f4a3b67d30c5738e4
ThiagoMRibeiro <thiagomorales@gmail.com>
2023-11-27 14:13:42 -0300

    add responsive

---
+++ php/src/assets/js/index.js
@@ -0,0 +1,25 @@
+const inputPassword = document.querySelector("#passwordUserLogin")
+const buttonShowHide = document.querySelector("#showPassword");
+var passwordIsHide = true;
+
+buttonShowHide.addEventListener("click", ()=>{
+    if(passwordIsHide){
+        showPassword();
+    } else {
+        hidePassword();
+    }
+});
+
+function showPassword(){
+    buttonShowHide.src = "../assets/images/icon/hide-password.png";
+    buttonShowHide.alt = "Esconder Senha (Hide Password)";
+    passwordIsHide = false;
+    inputPassword.type = "text";
+}
+
+function hidePassword(){
+    buttonShowHide.src = "../assets/images/icon/show-password.png";
+    buttonShowHide.alt = "Mostrar Senha (Show Password)";
+    passwordIsHide = true;
+    inputPassword.type = "password";
+}
