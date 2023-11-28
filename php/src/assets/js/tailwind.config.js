9a10ee5d0d914f5774f3c4ca1f09e7eec3ae627c
ThiagoMRibeiro <thiagomorales@gmail.com>
2023-11-28 20:17:33 -0300

    arranging the master

---
+++ php/src/assets/js/tailwind.config.js
@@ -0,0 +1,26 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            height: {
+                '100px': '100px',
+                'h-calc': 'calc(100vh - 100px)',
+            },
+            colors: {
+                'ycol': '#ECA72C',
+            },
+            width: {
+                'percent-70': '70%',
+            },
+            margin: {
+                'percent-10': '10%',
+            },
+            font: {
+                'Smythe': ['Smythe', 'sans-serif'],
+                'Inter': ['Inter', 'sans-serif'],
+                'Sancreek': ['Sancreek', 'serif'],
+            },
+
+        }
+    }
+}
