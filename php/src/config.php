d996e15d82308e8c3f028b54c60b17bcda576cfd
eytorsousa <eytorlima@outlook.com>
2023-12-01 00:38:13 -0300

    Index e +: identações e correções gerais

--- php/src/config.php
+++ php/src/config.php
@@ -12,7 +12,7 @@ try {
   $conn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  //echo "Connected successfully";
 
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
